e605de9652105c63cb39a2a9ab57e2b21b325223
Lucas Borges <lucasnsborges@gmail.com>
2018-11-21 17:53:03 -0300

    Updates

--- main.js
+++ main.js
@@ -40,8 +40,8 @@
       </div>
 
       <ul class="header-menu-items">
-        <li><a class="header-active" href="/public">Feature requests</a></li>
-        <li><a href="/public">Changelog</a></li>
+        <li><a class="header-active" href="#">Feature requests</a></li>
+        <li><a href="#">Changelog</a></li>
       </ul>
     </div>
   </header>
